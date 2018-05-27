eb4dd564ae960c1bcad3e8dc5141a2c95e8f0926
lbroll225972 <lbroll225972@e-science.pl>
2018-05-27 14:53:22 +0200

    Version 3

--- myPackage/__init__.py
+++ myPackage/__init__.py
@@ -1,2 +1,2 @@
 from . import somePython
-
+from . import fib
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='python-package',
-    version='2.1',
+    version='3.0',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
-    description='C to F grades converter',
-    long_description=open('README.md').read(),
+    description='temp converter & fibonacci counter',
+    long_description=open('README.txt').read(),
     install_requires=['numpy'],
-    url='https://git.e-science.pl/lbroll225972/lab10_python_package',
+    url='https://github.com/pluserovsky/python_package',
     author='Lukasz Broll',
     author_email='225972@student.pwr.edu.pl'
 )
