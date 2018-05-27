c1b6287e6da995c80c7999e3788c930ba502d58a
lbroll225972 <lbroll225972@e-science.pl>
2018-05-27 11:46:46 +0200

    Working package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python-package',
+    version='1.0',
+    packages=find_packages(exclude=['tests*']),
+    license='MIT',
+    description='C to F grades converter',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://git.e-science.pl/lbroll225972/lab10_python_package',
+    author='Lukasz Broll',
+    author_email='225972@student.pwr.edu.pl'
+)
